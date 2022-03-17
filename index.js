665436572700da0ad945a575a10202c37ae78b58
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-03-17 11:21:44 -0300

    :card_file_box: setup routes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
+
+app.use('/Categories', routes.Categories);
+app.use('/Login', routes.Login);
+app.use('Post', routes.Post);
+app.use('User', routes.User);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const Categories = require('./Categories');
+const Login = require('./Login');
+const Post = require('./Post');
+const User = require('./User');
+
+module.exports = {
+  Categories,
+  Login,
+  Post,
+  User,
+};
