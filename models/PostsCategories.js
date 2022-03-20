a2fa56a307526c0bbfb5c360cea5218663e7eead
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-03-20 13:49:17 -0300

    :wrench: fix postsCategories model

--- models/PostsCategories.js
+++ models/PostsCategories.js
@@ -1,21 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+const postsCategoriesAttributes = {
+  postId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+};
+
 module.exports = (sequelize, _DataTypes) => {
-  const PostsCategories = sequelize.define('PostsCategories', {}, { timestamps: false,
+  const PostsCategories = sequelize.define('PostsCategories', postsCategoriesAttributes, {
+    timestamps: false,
     tableName: 'PostsCategories',
   });
 
   PostsCategories.associate = (models) => {
     models.BlogPosts.belongsToMany(models.Categories, {
-      as: 'categories',
-      through: PostsCategories,
-      foreignKey: 'postId',
-      otherKey: 'categoryId',
+      as: 'categories', through: PostsCategories, foreignKey: 'postId', otherKey: 'categoryId',
     });
 
-    models.Categories.belongsTo(models.BlogPosts, {
-      as: 'posts',
-      through: PostsCategories,
-      foreignKey: 'categoryId',
-      otherKey: 'postId',
+    models.Categories.belongsToMany(models.BlogPosts, {
+      as: 'posts', through: PostsCategories, foreignKey: 'categoryId', otherKey: 'postId',
     });
   };
 
