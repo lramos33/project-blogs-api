1c3711d20fad1c444218a5ffd42556958b8b8e1a
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-03-20 23:35:34 -0300

    :card_file_box: improve JWT middleware pt2

--- services/User/index.js
+++ services/User/index.js
@@ -1,9 +1,11 @@
 const getAll = require('./getAll.service');
 const create = require('./create.service');
 const getById = require('./getById.service');
+const getByEmail = require('./getByEmail.service');
 
 module.exports = {
   getAll,
   create,
   getById,
+  getByEmail,
 };
